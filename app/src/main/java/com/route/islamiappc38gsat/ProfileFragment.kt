e45a06a18b7ae60020a4d429966ce87d32063cb8
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2023-08-05 10:13:33 +0300

    Developer 1 Worked On Feature 1 "Profile Fragment"

--- app/src/main/java/com/route/islamiappc38gsat/ProfileFragment.kt
+++ app/src/main/java/com/route/islamiappc38gsat/ProfileFragment.kt
@@ -2,6 +2,7 @@ package com.route.islamiappc38gsat
 
 import android.os.Bundle
 import android.text.Layout
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +18,12 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.e("TAG", "onViewCreated: Added OnViewCreated ")
+
     }
 
 
